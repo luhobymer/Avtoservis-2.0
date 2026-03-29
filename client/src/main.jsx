685190f6eeeaf4ec48a5afb7fa4316f9b84fd464
BMW <bmw@local>
2026-03-29 21:59:26 +0300

    Use HashRouter to prevent Cloudflare redirect issues

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
@@ -53,11 +53,11 @@ i18n.init().then(() => {
   try {
     root.render(
       <StrictMode>
-        <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+        <HashRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
           <AuthProvider>
             <App />
           </AuthProvider>
-        </BrowserRouter>
+        </HashRouter>
       </StrictMode>
     );
     console.log('Application mounted successfully');
